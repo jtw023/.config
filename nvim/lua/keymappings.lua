cfea59557d8ec81d80d068eb5fa0aa8c043222ef
jtw023 <r3stock3r@tutanota.com>
2021-04-19 22:40:43 -0700

    switched to lunarvim

---
+++ nvim/lua/keymappings.lua
@@ -0,0 +1,80 @@
+vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
+vim.g.mapleader = ' '
+
+-- no hl
+vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', {noremap = true, silent = true})
+
+-- explorer
+vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '-', ':RnvimrToggle<CR>', {noremap = true, silent = true})
+
+-- better window movement
+vim.api.nvim_set_keymap('n', '<S-h>', '<C-w>h', {silent = true})
+vim.api.nvim_set_keymap('n', '<S-j>', '<C-w>j', {silent = true})
+vim.api.nvim_set_keymap('n', '<S-k>', '<C-w>k', {silent = true})
+vim.api.nvim_set_keymap('n', '<S-l>', '<C-w>l', {silent = true})
+
+-- TODO fix this
+-- Terminal window navigation
+vim.cmd([[
+  tnoremap <C-H> <C-\><C-N><C-w>h
+  tnoremap <C-J> <C-\><C-N><C-w>j
+  tnoremap <C-K> <C-\><C-N><C-w>k
+  tnoremap <C-L> <C-\><C-N><C-w>l
+  inoremap <C-h> <C-\><C-N><C-w>h
+  inoremap <C-j> <C-\><C-N><C-w>j
+  inoremap <C-k> <C-\><C-N><C-w>k
+  inoremap <C-l> <C-\><C-N><C-w>l
+  tnoremap <Esc> <C-\><C-n>
+]])
+
+-- TODO fix this
+-- resize with arrows
+vim.cmd([[
+  nnoremap <silent> <C-Up>    :resize -2<CR>
+  nnoremap <silent> <C-Down>  :resize +2<CR>
+  nnoremap <silent> <C-Left>  :vertical resize -2<CR>
+  nnoremap <silent> <C-Right> :vertical resize +2<CR>
+]])
+
+-- better indenting
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
+
+-- I hate escape
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
+
+-- Exit shortcuts
+vim.api.nvim_set_keymap('n', '<S-f><S-i>', ':q!<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<S-f><S-i>', '<Esc>:q!<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-w><S-f>', ':x<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<S-w><S-f>', '<Esc>:x<CR>', {noremap = true, silent = true})
+vim.cmd([[
+	nnoremap <silent> <S-f>f :q!<CR>
+	inoremap <silent> <S-f>f <Esc><Esc>:q!<CR>
+]])
+
+-- Tab switch buffer
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
+
+-- Move selected line / block of text in visual mode
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
+
+-- Better nav for omnicomplete
+vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
+vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+-- vim.cmd('inoremap <expr> <TAB> (\"\\<C-n>\")')
+-- vim.cmd('inoremap <expr> <S-TAB> (\"\\<C-p>\")')
+
+-- vim.api.nvim_set_keymap('i', '<C-TAB>', 'compe#complete()', {noremap = true, silent = true, expr = true})
+
+-- vim.cmd([[
+-- map p <Plug>(miniyank-autoput)
+-- map P <Plug>(miniyank-autoPut)
+-- map <leader>n <Plug>(miniyank-cycle)
+-- map <leader>N <Plug>(miniyank-cycleback)
+-- ]])
