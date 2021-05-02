12cdde2af440abaab37e7b4bc5ff2541e28960db
jtw023 <r3stock3r@tutanota.com>
2021-05-02 14:11:31 -0700

    added 'devour' command to keybinding

--- qtile/config.py
+++ qtile/config.py
@@ -62,7 +62,7 @@ keys = [
     Key([mod, "shift"], "p", lazy.spawn("sxiv -t " + home + "/Pictures/")),
     Key([mod, "shift"], "d", lazy.spawn("rofi -show drun")),
     Key(["mod1", "shift"], "p", lazy.spawn(home + "/Random-Scripts/screenshot.sh")),
-    Key([mod, "shift"], "m", lazy.spawn("kitty -e /home/jordan/Random-Scripts/music_player.sh")),
+    Key([mod, "shift"], "m", lazy.spawn("kitty -e devour /home/jordan/Random-Scripts/music_player.sh")),
     Key([mod, "shift"], "q", lazy.window.kill()),
     Key([mod], "r", lazy.restart()),
     Key([mod, "shift"], "r", lazy.spawn(home + "/Random-Scripts/reboot.sh")),
