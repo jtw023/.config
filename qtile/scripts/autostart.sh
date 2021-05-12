866124a59d981c4d8a3416bb77910b8ccc231224
jtw023 <r3stock3r@tutanota.com>
2021-05-11 20:28:47 -0700

    added transmission-daemon to autostart

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
-# function run {
-#   if ! pgrep $1 ;
-#   then
-#     $@&
-#   fi
-# }
-
-
 #Programs to autostart
 
 redshift -l 36.114647:-115.172813 -O 1900 -m randr &
 lxsession &
 picom --config $HOME/.config/picom/picom.conf &
 dunst &
-# run volumeicon &
+transmission-daemon &
 nitrogen /usr/share/backgrounds/5760x1080/ --random --set-scaled
