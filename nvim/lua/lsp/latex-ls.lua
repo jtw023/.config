6a0be867a6c8c306a2c331c8eabe1ffdcc956429
jtw023 <r3stock3r@tutanota.com>
2021-05-25 23:26:57 -0700

    reset cache

--- nvim/lua/lsp/latex-ls.lua
+++
@@ -1,4 +0,0 @@
-require'lspconfig'.texlab.setup{
-    cmd = {"/home/jordan/.local/share/nvim/lspinstall/latex/texlab"},
-    on_attach = require'lsp'.common_on_attach
-}
--- nvim/lua/lsp/terraform-ls.lua
+++
@@ -1,4 +0,0 @@
-require'lspconfig'.terraformls.setup{
-    cmd = {"/home/jordan/.local/share/nvim/lspinstall/terraform/terraform-ls", "serve"},
-    on_attach = require'lsp'.common_on_attach
-}
