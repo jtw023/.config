c43b095395866a3b1aa4219917140abf0c25dc21
jtw023 <r3stock3r@tutanota.com>
2022-05-17 16:30:21 -0700

    created autocommands and new plugins

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,16 +1,16 @@
 local group = vim.api.nvim_create_augroup("Run_File", { clear = true })
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*.py",
-	command = [[nmap <A-r> :w \| :!time /usr/bin/python3 %<CR>]],
+	command = [[nmap <A-r> :w \| :!time python3 %<CR>]],
 	group = group
 })
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*.js",
-	command = [[nmap <A-r> :w \| :!time /usr/bin/node %<CR>]],
+	command = [[nmap <A-r> :w \| :!time node %<CR>]],
 	group = group
 })
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*.sh",
-	command = [[nmap <A-r> :w \| :!time /usr/bin/sh %<CR>]],
+	command = [[nmap <A-r> :w \| :!time sh %<CR>]],
 	group = group
 })
