2f9cac04058df57948c0125921af88dcc5df9558
jtw023 <r3stock3r@tutanota.com>
2022-06-06 10:00:58 -0700

    Modifications to snippets and treesitter

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -6,7 +6,7 @@ local insert = ls.insert_node
 
 -- ls.parser.parse_snippet(<text>, <VS**** style snippet>)
 ls.add_snippets(nil, {
-	vimwiki = {
+	all = {
 		snip({
 			trig = 'todo',
 			name = 'Todo',
@@ -58,48 +58,6 @@ ls.add_snippets(nil, {
 		}),
 	},
 	python = {
-		snip({
-			trig = 'todo',
-			name = 'Todo',
-			dscr = 'Todo Comment',
-		}, {
-			text({'# TODO: '}), insert(1, 'task'),
-		}),
-		snip({
-			trig = 'note',
-			name = 'Note',
-			dscr = 'Note Comment',
-		}, {
-			text({'# NOTE: '}), insert(1, 'note'),
-		}),
-		snip({
-			trig = 'fix',
-			name = 'Fix',
-			dscr = 'Fix Comment',
-		}, {
-			text({'# FIX: '}), insert(1, 'fix'),
-		}),
-		snip({
-			trig = 'hack',
-			name = 'Hack',
-			dscr = 'Hack Comment',
-		}, {
-			text({'# HACK: '}), insert(1, 'hack'),
-		}),
-		snip({
-			trig = 'question',
-			name = 'Question',
-			dscr = 'Question',
-		}, {
-			text({'# QUESTION: '}), insert(1, 'question'),
-		}),
-		snip({
-			trig = 'caveat',
-			name = 'Caveat',
-			dscr = 'Caveat',
-		}, {
-			text({'CAVEAT: '}), insert(1, 'caveat'),
-		}),
 		snip({
 			trig = 'print',
 			name = 'Print Function',
