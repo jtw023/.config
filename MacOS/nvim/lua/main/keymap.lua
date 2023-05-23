17e586e11a96ba8e98ab315e644f6250871a7aa8
Jordan Walters <jordan.walters@reflexmedia.com>
2023-05-22 20:57:28 -0700

    Added description to all keymaps

--- MacOS/nvim/lua/main/keymap.lua
+++ MacOS/nvim/lua/main/keymap.lua
@@ -6,91 +6,122 @@ local opts = { noremap = true, silent = true }
 -- Unmap and remap Leader key
 vim.g.mapleader = ' '
 -- Turn off highlighting
+opts.desc = 'Turn Off Highlighting'
 vim.keymap.set('n', '<Leader>2', ':nohlsearch<CR>', opts)
 -- Cheat sheet
+opts.desc = 'Open Keymap File In New Tab'
 vim.keymap.set('n', '<Leader>3', ':w <BAR> :tabnew $HOME/.config/nvim/lua/main/keymap.lua<CR>', opts)
 -- Delete and save to void register
+opts.desc = 'Paste To Register Without Overwriting Clipboard'
 vim.keymap.set('v', '<Leader>p', [["_dP]], opts)
 -- Expand JSON
+opts.desc = 'Expand JSON'
 vim.keymap.set('n', '<C-e>', ':%!python -m json.tool<CR>', opts)
--- Use <TAB> to select the popup menu:
-vim.keymap.set('i', '<TAB>', 'pumvisible() ? "<C-n>" : "<TAB>"', { expr = true, noremap = true, silent = true })
-vim.keymap.set('i', '<S-TAB>', 'pumvisible() ? "<C-p>" : "<S-TAB>"', { expr = true, noremap = true, silent = true })
 -- FoldEnd
 
 -- FoldStart
 -- Enter and Exit
 -- Save and quit
+opts.desc = 'Save And Exit All Files'
 vim.keymap.set('n', '<Leader>4', ':xa<CR>', opts)
 -- Quit and exit
+opts.desc = 'Force Quit All Files'
 vim.keymap.set('n', '<Leader>8', ':qa!<CR>', opts)
 -- FoldEnd
 
 -- FoldStart
 -- Dadbod
 -- Toggle Drawer
+opts.desc = 'Open DBUI Drawer'
 vim.keymap.set('n', '<Leader>o', ':DBUIToggle<CR>', opts)
 -- Launch Program
-vim.keymap.set('n', '<Leader>s', ':DBUI<CR>', opts)
+opts.desc = 'Open DBUI Program'
+vim.keymap.set('n', '<S-p>', ':DBUI<CR>', opts)
 -- FoldEnd
 
 -- FoldStart
 -- Create newline above or below
+opts.desc = 'Add New Line Above Cursor And Stay In Normal Mode'
 vim.keymap.set('n', '<S-u>', '<S-o><ESC>', opts)
+opts.desc = 'Add New Line Below Cursor And Stay In Normal Mode'
 vim.keymap.set('n', '<S-b>', 'o<ESC>', opts)
 -- FoldEnd
 
 -- FoldStart
 -- Fold Lines
 -- Create a new fold
+opts.desc = 'Create New Fold'
 vim.keymap.set({'n', 'v'}, '<S-z>', 'zf', opts)
 -- Toggle the fold
+opts.desc = 'Toggle Fold Between Open And Closed'
 vim.keymap.set('n', '<S-m>', 'za', opts)
 -- Delete the fold
+opts.desc = 'Delete Current Fold'
 vim.keymap.set('n', '<S-x>', 'zd', opts)
 -- FoldEnd
 
 -- FoldStart
 -- Rename and Spelling
 -- Fix spelling
+opts.desc = 'Open Telescope Spell Suggest'
 vim.keymap.set('n', '<S-s>', ':Telescope spell_suggest<CR>', opts)
 -- Rename all instances in file of word under cursor
+opts.desc = 'Rename All Instances In File Of Word Under Cursor'
 vim.keymap.set('n', '<Leader>r', ':%s/<C-r><C-w>/', opts)
 -- FoldEnd
 
 -- FoldStart
 -- Control Windows
 -- Change window
+opts.desc = 'Change To Window To The Right'
 vim.keymap.set('n', '<Leader>l', '<C-w>l', opts) -- right
+opts.desc = 'Change To Above Window'
 vim.keymap.set('n', '<Leader>k', '<C-w>k', opts) -- up
-vim.keymap.set('n', '<Leader>j', '<C-w>j', opts) -- down
-vim.keymap.set('n', '<Leader>h', '<C-w>h', opts) -- left
+opts.desc = 'Change To Below Window'
+vim.keymap.set('n', '<Leader>j', '<C-w>j', opts ) -- down
+opts.desc = 'Change To Window To The Left'
+vim.keymap.set('n', '<Leader>h', '<C-w>h', opts ) -- left
 -- Jump Scroll
+opts.desc = 'Jump Scroll To The Right'
 vim.keymap.set({'n', 'v'}, '<S-l>', 'zL', opts) -- jump right
+opts.desc = 'Jump Scroll To The Left'
 vim.keymap.set({'n', 'v'}, '<S-h>', 'zH', opts) -- jump left
+opts.desc = 'Jump Up Half Screen'
 vim.keymap.set({'n', 'v'}, '<S-k>', '<C-u>', opts) -- jump up
+opts.desc = 'Jump Down Half Screen'
 vim.keymap.set({'n', 'v'}, '<S-j>', '<C-d>', opts) -- jump down
 -- Split screen
-vim.keymap.set('n', '<Leader>vs', ':vs .<CR>', opts)
+opts.desc = 'Open Splitscreen In Netrw'
+vim.keymap.set('n', '<Leader>s', ':vs .<CR>', opts)
 -- Resize buffers
-vim.keymap.set('n', '<S-Up>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<S-Down>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<S-Right>', ':vertical resize +2<CR>', opts)
-vim.keymap.set('n', '<S-Left>', ':vertical resize -2<CR>', opts)
+opts.desc = 'Mover Edge of Buffer Window Up'
+vim.keymap.set('n', '<S-Up>', ':resize +2<CR>', opts) -- up
+opts.desc = 'Mover Edge of Buffer Window Down'
+vim.keymap.set('n', '<S-Down>', ':resize -2<CR>', opts) -- down
+opts.desc = 'Mover Edge of Buffer Window Right'
+vim.keymap.set('n', '<S-Right>', ':vertical resize +2<CR>', opts) -- right
+opts.desc = 'Mover Edge of Buffer Window Left'
+vim.keymap.set('n', '<S-Left>', ':vertical resize -2<CR>', opts) -- left
 -- Switch vertical split to horizontal split
-vim.keymap.set('n', '<Leader>hr', '<C-w>L', opts) -- horizontal
-vim.keymap.set('n', '<Leader>vr', '<C-w>J', opts) -- vertical
+opts.desc = 'Switch Vertical Split to Horizontal Split'
+vim.keymap.set('n', '<Leader>u', '<C-w>L', opts) -- horizontal
+opts.desc = 'Switch Horizontal Split to Vertical Split'
+vim.keymap.set('n', '<Leader>v', '<C-w>J', opts) -- vertical
 -- Switch Tabs
 -- <Leader>b opens a list of all buffers
 -- 'gd' jumps to definition of any word in any file(python or lua only)
+opts.desc = 'Return To Previous Buffer After Jump To Definition'
 vim.keymap.set('n', 'gb', ':w <BAR> :norm <C-t><CR>', opts) -- return to previous buffer after using 'gd'
+opts.desc = 'Go To Next Already Open Buffer'
 vim.keymap.set('n', '>', ':w <BAR> :bnext<CR>', opts) -- go to next buffer
+opts.desc = 'Return To Previous Already Open Buffer'
 vim.keymap.set('n', '<', ':w <BAR> :bprev<CR>', opts) -- return to previous buffer
 -- FoldEnd
 
 -- FoldStart
 -- Explorer
 -- Toggle file explorer with Leader-m
+opts.desc = 'Toggle Netrw Window'
 vim.keymap.set('n', '<Leader>m', function()
 	vim.cmd([[
 		if exists("t:expl_buf_num")
@@ -116,42 +147,56 @@ end, opts)
 -- FoldStart
 -- Telescope
 -- Show all TODOs
+opts.desc = 'See TODOs In Project'
 vim.keymap.set('n', '<Leader>td', ':exe ":w <BAR> :TodoTelescope search_dirs=" .. fnameescape(expand("%:p"))<CR>', opts)
 -- Fuzzy find keymaps
+opts.desc = 'Fuzzy Find Keymaps'
 vim.keymap.set('n', '<Leader>tk', ':w <BAR> :Telescope keymaps<CR>', opts)
 -- Show Buffer List
+opts.desc = 'Open Buffer List'
 vim.keymap.set('n', '<Leader>b', ':w <BAR> :Telescope buffers<CR>', opts)
 -- Close Current Buffer
+opts.desc = 'Close Current Buffer'
 vim.keymap.set('n', '<Leader>c', ':bd<CR>', opts)
 -- Find Files
-vim.keymap.set('n', '<Leader>ff', ':w <BAR> :Telescope find_files hidden=true<CR>', opts)
+opts.desc = 'Find Files With Telescope'
+vim.keymap.set('n', '<Leader>tf', ':w <BAR> :Telescope find_files hidden=true<CR>', opts)
 -- Find Recent Files
-vim.keymap.set('n', '<Leader>fh', ':w <BAR> :Telescope oldfiles<CR>', opts)
+opts.desc = 'Find Recently Opened Files With Telescope'
+vim.keymap.set('n', '<Leader>to', ':w <BAR> :Telescope oldfiles<CR>', opts)
 -- Find Git Files
-vim.keymap.set('n', '<Leader>fg', ':w <BAR> :Telescope git_files<CR>', opts)
--- Find Keymaps
-vim.keymap.set('n', '<Leader>fk', ':w <BAR> :Telescope keymaps<CR>', opts)
+opts.desc = 'Find Git Files With Telescope'
+vim.keymap.set('n', '<Leader>tg', ':w <BAR> :Telescope git_files<CR>', opts)
 -- Live Grep
-vim.keymap.set('n', '<Leader>fl', ':exe "w <BAR> :Telescope live_grep cwd=" .. fnameescape(expand("%:p:h"))<CR>', opts)
+opts.desc = 'Live Grep Files With Telescope'
+vim.keymap.set('n', '<Leader>tl', ':exe "w <BAR> :Telescope live_grep cwd=" .. fnameescape(expand("%:p:h"))<CR>', opts)
 -- View variables
-vim.keymap.set('n', '<Leader>fv', ':w <BAR> :Telescope lsp_document_symbols<CR>', opts)
+opts.desc = 'Find Variables With Telescope'
+vim.keymap.set('n', '<Leader>tv', ':w <BAR> :Telescope lsp_document_symbols<CR>', opts)
 -- View Help Tags
-vim.keymap.set('n', '<Leader>hh', ':w <BAR> :Telescope help_tags<CR>', opts)
+opts.desc = 'Nvim Help With Telescope'
+vim.keymap.set('n', '<Leader>th', ':w <BAR> :Telescope help_tags<CR>', opts)
 -- FoldEnd
 
 -- FoldStart
 -- Line Manipulation
 -- Move selected line up or down
 					  -- :'<,'>s/\w\+/"\0"/g
+opts.desc = 'Move Selected Line Up'
 vim.keymap.set('v', '<S-u>', ":m '<-2<CR>gv=gv", opts)
+opts.desc = 'Move Selected Line Down'
 vim.keymap.set('v', '<S-b>', ":m '>+1<CR>gv=gv", opts)
 -- Indent selected line in or out
+opts.desc = 'Unindent Selected Line'
 vim.keymap.set('v', '<Leader>,', '<<<ESC>gv', opts)
+opts.desc = 'Indent Selected Line'
 vim.keymap.set('v', '<Leader>.', '>><ESC>gv', opts)
 -- Comment toggle current line or selected line
+opts.desc = 'Toggle Comments'
 vim.keymap.set('n', '<Leader>/', ':CommentToggle<CR><ESC>', opts)
 vim.keymap.set('v', '<Leader>/', ':CommentToggle<CR>"<ESC>', opts)
 -- Surround every word under cursor or every word in selection
+opts.desc = 'Wrap Words'
 vim.keymap.set('n', "'<S-w>", "viwc''<ESC>hp", opts) -- word
 vim.keymap.set('n', '"<S-w>', 'viwc""<ESC>hp', opts) -- word
 vim.keymap.set('n', '`<S-w>', 'viwc``<ESC>hp', opts) -- word
@@ -165,19 +210,28 @@ vim.cmd([[vnoremap {<S-w> :s/\w\+/{\0}/g<CR> \| :noh<CR>]]) -- line
 vim.cmd([[vnoremap [<S-w> :s/\w\+/[\0]/g<CR> \| :noh<CR>]]) -- line
 vim.cmd([[vnoremap (<S-w> :s/\w\+/(\0)/g<CR> \| :noh<CR>]]) -- line
 -- Invert Character Case
+opts.desc = 'Invert Character Case'
 vim.keymap.set('n', '<S-c>', 'v~', opts)
 vim.keymap.set('v', '<S-c>', '~', opts)
 -- FoldEnd
 
 -- FoldStart
 -- Git -- Show diff or jump to added[+], modified[->], or deleted[-] line
+opts.desc = 'Preview Gitsigns Hunk'
 vim.keymap.set('n', '<Leader>gv', ':Gitsigns preview_hunk<CR>', opts)
+opts.desc = 'Next Gitsigns Hunk'
 vim.keymap.set('n', '<Leader>gf', ':Gitsigns next_hunk<CR>', opts)
+opts.desc = 'Previous Gitsigns Hunk'
 vim.keymap.set('n', '<Leader>gb', ':Gitsigns prev_hunk<CR>', opts)
+opts.desc = 'View Diffsplit'
 vim.keymap.set('n', '<Leader>d', ':diffsplit<CR>', opts)
+opts.desc = 'Open Git Buffer'
 vim.keymap.set('n', 'gs', ':w <BAR> :Git<CR>', opts)
+opts.desc = 'View Git Log'
 vim.keymap.set('n', 'gl', ':Git log<CR>', opts)
+opts.desc = 'Make Git Push'
 vim.keymap.set('n', 'gpush', ':Git push<CR>', opts)
+opts.desc = 'Make Git Pull'
 vim.keymap.set('n', 'gpull', ':Git pull<CR>', opts)
 -- FoldEnd
 
