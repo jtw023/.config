4c9dfffbd0fadbd56dbf128ffd3b6b058956e5cf
Jordan Walters <R3stock3r@tutanota.com>
2023-06-16 10:24:31 -0700

    Added new group names and reorganized files

--- MacOS/nvim/lua/config/autocommands.lua
+++ MacOS/nvim/lua/config/autocommands.lua
@@ -1,88 +1,91 @@
-local group1 = vim.api.nvim_create_augroup("Run_File", { clear = true })
-local group2 = vim.api.nvim_create_augroup("Save_File", { clear = true })
-local group3 = vim.api.nvim_create_augroup("Completion", { clear = true })
+local runFile = vim.api.nvim_create_augroup("Run_File", { clear = true })
+local nvimOptions = vim.api.nvim_create_augroup("Nvim_Options", { clear = true })
+local formatNvim = vim.api.nvim_create_augroup("Format_Nvim", { clear = true })
+local onLeave = vim.api.nvim_create_augroup("On_Leave", { clear = true })
+local expandKey = vim.api.nvim_create_augroup("Expand_Key", { clear = true })
+
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.py",
 	command = [[nmap <S-r> :w \| :!time python3 %<CR>]],
-	group = group1
+	group = runFile
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.js",
 	command = [[nmap <S-r> :w \| :!time node %<CR>]],
-	group = group1
+	group = runFile
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.sh",
 	command = [[nmap <S-r> :w \| :!time sh %<CR>]],
-	group = group1
+	group = runFile
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.sql",
 	command = [[nmap <S-r> <Leader><S-s>]],
-	group = group1
+	group = runFile
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.sql",
 	command = [[:set nocursorline]],
-	group = group1
-})
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-	pattern = { "*.sql", "*.lua" },
-	command = [[:iab * -- *| :iab tod -- TODO:| :iab fi -- FIX:| :iab inp -- INPROGRESS:| :iab que -- QUESTION:| :iab cav -- CAVEAT:| :iab rol -- ROLLUP_KEYS:| :iab tit -- TITLE:| :iab lin -- LINK:| :iab abo -- ABOUT:]],
-	group = group1
-})
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-	pattern = "*.py",
-	command = [[:iab * # *| :iab tod # TODO:| :iab fi # FIX:| :iab inp # INPROGRESS:| :iab que # QUESTION:| :iab cav # CAVEAT:| :iab rol # ROLLUP_KEYS:| :iab tit # TITLE:| :iab lin # LINK:| :iab abo # ABOUT:]],
-	group = group1
+	group = nvimOptions
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.py",
 	command = [[:set foldmarker=#FoldStart,#FoldEnd]],
-	group = group1
+	group = nvimOptions
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = { "*.sql", "*.lua" },
 	command = [[:set foldmarker=--\ FoldStart,--\ FoldEnd]],
-	group = group1
+	group = nvimOptions
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.vim",
 	command = [[:set foldmarker=\"FoldStart,\"FoldEnd]],
-	group = group1
+	group = nvimOptions
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.sql",
 	command = "lua vim.api.nvim_buf_set_option(0, 'commentstring', '-- %s')",
-	group = group1
+	group = nvimOptions
 })
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
 	pattern = "*.sqlfluff",
 	command = "lua vim.api.nvim_buf_set_option(0, 'commentstring', '# %s')",
-	group = group1
-})
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-	pattern = "*.sql",
-	command = [[nmap <Leader>= :w \| :silent !sqlfluff format --config /Users/jordanwalters/bitbucket_repos/jordanw/SQL/.sqlfluff % >/dev/null 2>&1<CR>]],
-	group = group1
-})
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-	pattern = { "*.py", "*.lua" },
-	command = [[nmap <Leader>= :w \| :lua vim.lsp.buf.format { async = true }<CR>]],
-	group = group1
+	group = nvimOptions
 })
 vim.api.nvim_create_autocmd({ "BufRead", "BufEnter", "BufWinEnter"}, {
     pattern = "*/bitbucket_repos/jordanw/SQL/*",
     command = [[ :set undofile ]],
-    group = group1
-})
-vim.api.nvim_create_autocmd({"BufLeave", "BufWinLeave"}, {
-	pattern = "*.md",
-	command = [[ :silent ! osascript $HOME/github_repos/Random-Scripts/markdown_close.scpt ]],
-	group = group2
+    group = nvimOptions
 })
 vim.api.nvim_create_autocmd({"FileType"}, {
 	pattern = { "sql", "mysql", "plsql" },
 	command = "lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }}})",
-	group = group3
+	group = nvimOptions
+})
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+	pattern = { "*.sql", "*.lua" },
+	command = [[:iab * -- *| :iab tod -- TODO:| :iab fi -- FIX:| :iab inp -- INPROGRESS:| :iab que -- QUESTION:| :iab cav -- CAVEAT:| :iab rol -- ROLLUP_KEYS:| :iab tit -- TITLE:| :iab lin -- LINK:| :iab abo -- ABOUT:]],
+	group = expandKey
+})
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+	pattern = "*.py",
+	command = [[:iab * # *| :iab tod # TODO:| :iab fi # FIX:| :iab inp # INPROGRESS:| :iab que # QUESTION:| :iab cav # CAVEAT:| :iab rol # ROLLUP_KEYS:| :iab tit # TITLE:| :iab lin # LINK:| :iab abo # ABOUT:]],
+	group = expandKey
+})
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+	pattern = "*.sql",
+	command = [[nmap <Leader>= :w \| :silent !sqlfluff format --config /Users/jordanwalters/bitbucket_repos/jordanw/SQL/.sqlfluff % >/dev/null 2>&1<CR>]],
+	group = formatNvim
+})
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+	pattern = { "*.py", "*.lua" },
+	command = [[nmap <Leader>= :w \| :lua vim.lsp.buf.format { async = true }<CR>]],
+	group = formatNvim
+})
+vim.api.nvim_create_autocmd({"BufLeave", "BufWinLeave"}, {
+	pattern = "*.md",
+	command = [[ :silent ! osascript $HOME/github_repos/Random-Scripts/markdown_close.scpt ]],
+	group = onLeave
 })
