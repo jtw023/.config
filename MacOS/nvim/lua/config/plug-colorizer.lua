7242a746d79a898efb820891c530d02beab5ae7e
Jordan Walters <R3stock3r@tutanota.com>
2023-06-18 01:02:23 -0700

    Fixed Formatting

--- MacOS/nvim/lua/config/plug-colorizer.lua
+++ MacOS/nvim/lua/config/plug-colorizer.lua
@@ -5,12 +5,13 @@ if not status_ok then
 end
 
 colorizer.setup(
-  {'sql', 'python', 'lua';},
-  {
-	RGB      = true;         -- #RGB hex codes
-	RRGGBB   = true;         -- #RRGGBB hex codes
-	names    = true;         -- "Name" codes like Blue
-	RRGGBBAA = true;         -- #RRGGBBAA hex codes
-	rgb_fn   = true;         -- CSS rgb() and rgba() functions
-	hsl_fn   = true;         -- CSS hsl() and hsla() functions
-  })
+    {'sql', 'python', 'lua';},
+    {
+        RGB      = true;         -- #RGB hex codes
+        RRGGBB   = true;         -- #RRGGBB hex codes
+        names    = true;         -- "Name" codes like Blue
+        RRGGBBAA = true;         -- #RRGGBBAA hex codes
+        rgb_fn   = true;         -- CSS rgb() and rgba() functions
+        hsl_fn   = true;         -- CSS hsl() and hsla() functions
+    }
+)
