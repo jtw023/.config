3598f74bb2d55f2cd39d7204ab6912237d4f98ec
jtw023 <R3stock3r@tutanota.com>
2024-05-02 16:17:47 -0700

    deleted old files

--- Linux/nvim/lua/plug-nerdtree.lua
+++
@@ -1,7 +0,0 @@
-vim.g.NERDTreeQuitOnOpen = 1
-vim.g.NERDTreeShowHidden = 1
-vim.g.NERDTreeFileExtensionHighlightFullName = 1
-vim.g.NERDTreeLimitedSyntax = 1
-vim.g.NERDTreeAutoDeleteBuffer = 1
-vim.g.NERDTreeDirArrowExpandable = ''
-vim.g.NERDTreeDirArrowCollapsible = ''
--- Linux/superpaper/profiles/jordan.profile
+++
@@ -1,5 +0,0 @@
-name=jordan
-spanmode=advanced
-slideshow=False
-perspective=default
-display0paths=/home/jordan/wallpapers
