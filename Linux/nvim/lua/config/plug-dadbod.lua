76a0cf856faf675e6346fa23b5d38d7271c8fdaa
jtw023 <R3stock3r@tutanota.com>
2024-05-28 01:06:13 +0200

    Added TODO comment

--- Linux/nvim/lua/config/plug-dadbod.lua
+++ Linux/nvim/lua/config/plug-dadbod.lua
@@ -1,3 +1,8 @@
+-- TODO: Make pull request to change redshift schemas, tables, and columns checks on dadbod-ui-completion
+-- nvim/autoload/plugged/vim-dadbod-completion/autoload/vim_dadbod_completion/schemas.vim
+-- let s:base_column_query = 'SELECT DISTINCT TABLE_NAME,COLUMN_NAME FROM SVV_COLUMNS'
+-- let s:schema_query = 'SELECT TABLE_SCHEMA,TABLE_NAME FROM SVV_COLUMNS GROUP BY TABLE_SCHEMA,TABLE_NAME'
+
 vim.g.db_ui_disable_progress_bar = 1
 vim.g.db_ui_use_postgres_views = 0
 vim.g.db_ui_force_echo_notifications = 1
